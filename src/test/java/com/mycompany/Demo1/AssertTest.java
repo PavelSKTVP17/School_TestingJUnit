535254dc8d487ec03a3e6f24e0836981139f623b
user <user@kab305-11>
2019-05-20 19:58:19 +0300

    extended 1

--- src/test/java/com/mycompany/Demo1/AssertTest.java
+++ src/test/java/com/mycompany/Demo1/AssertTest.java
@@ -11,6 +11,7 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.Ignore;
 
 /**
  *
@@ -35,15 +36,16 @@ public class AssertTest
     public void tearDown() {
     }
     */
-    @Test
+    @Test 
+    @Ignore
     public void substring() 
     {
         assertEquals("llo", "Hello".substring(3));
     }
-    @Test
+    /*@Test
     public void demo_test() 
     {
         assertEquals("llo", "Hel"+"lo");
         assertEquals(1.0, 1d/3+1d/3+1d/3,1e-10);
-    }
+    }*/
 }
